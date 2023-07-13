606cf601bfc6ecb6c26eee80671effb1381485d6
Manonthemon <klimowicz.maciej@gmail.com>
2023-07-13 09:25:41 +0100

    Added function to display button value on display when clicked. Added display clearing with C click

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
-console.log("Hello World")
+const display = document.getElementById('display');
+
+// Function to handle button clicks
+function handleButtonClick(event) {
+  const clickedButton = event.target;
+  const buttonText = clickedButton.textContent;
+  
+  // Append the clicked button's text to the display
+  display.value += buttonText;
+}
+
+// Attach click event listeners to all buttons
+const buttons = document.querySelectorAll('#num-pad button');
+buttons.forEach(button => {
+  button.addEventListener('click', handleButtonClick);
+});
+
+
+const clear = document.getElementById('clear');
+
+// Function to handle C button click
+function handleCClick(event) {
+  const cButton = event.target;
+  // Append the clicked button's text to the display
+  display.value = ""
+}
+
+clear.addEventListener('click' , handleCClick )
