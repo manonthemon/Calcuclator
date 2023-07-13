b06cca7bed5e7bdc32b235d84e6ac73305e87344
Manonthemon <klimowicz.maciej@gmail.com>
2023-07-13 13:32:56 +0100

    Added fuction to make calculations using user input

--- scripts.js
+++ scripts.js
@@ -1,28 +1,38 @@
-const display = document.getElementById('display');
+const display = document.getElementById("display");
 
-// Function to handle button clicks
+// Function to handle number button clicks
 function handleButtonClick(event) {
   const clickedButton = event.target;
   const buttonText = clickedButton.textContent;
-  
+
   // Append the clicked button's text to the display
   display.value += buttonText;
 }
 
-// Attach click event listeners to all buttons
-const buttons = document.querySelectorAll('#num-pad button');
-buttons.forEach(button => {
-  button.addEventListener('click', handleButtonClick);
+// Attach click event listeners to all number buttons
+const buttons = document.querySelectorAll(".num-button");
+buttons.forEach((button) => {
+  button.addEventListener("click", handleButtonClick);
 });
 
-
-const clear = document.getElementById('clear');
-
-// Function to handle C button click
-function handleCClick(event) {
-  const cButton = event.target;
-  // Append the clicked button's text to the display
-  display.value = ""
+//Function to make calculation and display results
+function calculate() {
+  resultValue = display.value;
+  var result = eval(resultValue);
+  display.value = result;
 }
 
-clear.addEventListener('click' , handleCClick )
+//event listener for equals button
+const equals = document.getElementById("equals");
+function handleCalculate() {
+  console.log(display.value);
+}
+equals.addEventListener("click", calculate);
+
+// Function to handle C button click
+const clear = document.getElementById("clear");
+function handleCClick(event) {
+  // Append the clicked button's text to the display
+  display.value = "";
+}
+clear.addEventListener("click", handleCClick);
