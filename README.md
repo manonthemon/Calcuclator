bc535a0dc32e1596f32a472a7812dffb7817a4cb
Manonthemon <klimowicz.maciej@gmail.com>
2023-07-13 14:17:51 +0100

    Replaced all functions with arrow functions, added readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Calcuclator
+# Calculator
+
+This is a simple on-screen calculator app. I made it to practice using HTML, CSS and JS. It was made using the following elements:
+
+- CSS Flex
+- CSS Grid
+- Event listeners
+- Query selectors
+- For each loops
+- Arrow functions
+- Eval function
+
+## Screenshot
+
+<img src="./screenshot.jpg" alt="Calculator Screenshot" width="300" height="300">
+
+## Deployment
+
+The deployed app can be tested here: https://manonthemon.github.io/calculator
+
+## Contributors
+
+I coded this app myself, I avoided using tutorials. For more information contact me at klimowicz.maciej@gmail.com
+
--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const display = document.getElementById("display");
 
 // Function to handle number button clicks
-function handleButtonClick(event) {
+const handleButtonClick = (event) => {
   const clickedButton = event.target;
   const buttonText = clickedButton.textContent;
 
@@ -16,22 +16,22 @@ buttons.forEach((button) => {
 });
 
 //Function to make calculation and display results
-function calculate() {
+const calculate = () => {
   resultValue = display.value;
-  var result = eval(resultValue);
+  let result = eval(resultValue);
   display.value = result;
-}
+};
 
 //event listener for equals button
 const equals = document.getElementById("equals");
-function handleCalculate() {
+const handleCalculate = () => {
   console.log(display.value);
 }
 equals.addEventListener("click", calculate);
 
 // Function to handle C button click
 const clear = document.getElementById("clear");
-function handleCClick(event) {
+const handleCClick = () => {
   // Append the clicked button's text to the display
   display.value = "";
 }
